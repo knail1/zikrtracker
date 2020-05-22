1d6c98719dde099d1cb08f27902ec98c02eb05d5
Dawud Rehan <d.rehan7@gmail.com>
2020-05-22 00:33:53 -0400

    displaying database working

--- app.py
+++ app.py
@@ -11,8 +11,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://kmdvxvuvocjhha:ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d8hto9mvtubuln'
 
+
+
+
 @app.route('/', methods = ['GET', 'POST'])
 def index():
+    
     if request.method == 'GET':
         pass
 
@@ -20,7 +24,7 @@ def index():
         name = request.form.get('name')
         date = request.form.get('date')
         comment = request.form.get('comment')
-        
+        create_post(date, name, comment)
     posts = get_posts()
 
     return render_template('index.html', posts=posts)
--- models.py
+++ models.py
@@ -2,7 +2,8 @@ import psycopg2
 from app import app
 
 
-def create_post(name, date, comment):
+def create_table():
+
     conn = psycopg2.connect(    
             host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
             database = 'd8hto9mvtubuln',
@@ -11,8 +12,23 @@ def create_post(name, date, comment):
             port = 5432
     )
     cur = conn.cursor()
-    cur.execute('INSERT INTO testdb (date, name, commment) VALUES (now(), ?, ?)', (name, comment))
+    cur.execute('CREATE TABLE IF NOT EXISTS testdb (id BIGSERIAL PRIMARY KEY, date DATE NOT NULL, name VARCHAR(150) NOT NULL, comment VARCHAR(150) NOT NULL)')
+    conn.commit()
+    conn.close()
 
+
+
+
+def create_post(date, name, comment):
+    conn = psycopg2.connect(    
+            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
+            database = 'd8hto9mvtubuln',
+            user = 'kmdvxvuvocjhha',
+            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
+            port = 5432
+    )
+    cur = conn.cursor()
+    cur.execute("INSERT INTO testdb (date, name, comment) VALUES (%s, %s, %s)", ('NOW()', name, comment)) 
     conn.commit()
 
     conn.close()
