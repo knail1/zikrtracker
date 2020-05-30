b902a3bcc7e2c0c0b019b684e2019ba8619c58af
Dawud Rehan <d.rehan7@gmail.com>
2020-05-30 14:25:20 -0400

    security

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy 
-from flask_cors import CORS
+import os
 from models import *
 import psycopg2
 
@@ -9,7 +9,7 @@ import psycopg2
 app = Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://kmdvxvuvocjhha:ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d8hto9mvtubuln'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'os.environ['DATABASE_URL']
 
 
 @app.route('/clear')
--- models.py
+++ models.py
@@ -7,11 +7,10 @@ import psycopg2
 
 def delete_table():
     conn = psycopg2.connect(    
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
-            database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            host = os.environ['HOST_NAME'],
+            user = os.environ['USERNAME'],
+            password = os.environ['PASSWORD']
+            port = os.environ['PORT']
     )
     cur = conn.cursor()
     cur.execute("DELETE FROM testdb")
