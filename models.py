18a41943722951d0a7683eeb53b9afbae05a9345
Dawud Rehan <d.rehan7@gmail.com>
2020-05-30 14:28:41 -0400

    security

--- models.py
+++ models.py
@@ -23,11 +23,11 @@ def delete_table():
 def create_table():
 
     conn = psycopg2.connect(    
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
+            host = os.environ['HOST_NAME'],
             database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            user = os.environ['USERNAME'],
+            password = os.environ['PASSWORD']
+            port = os.environ['PORT']
     )
     cur = conn.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS testdb (id BIGSERIAL PRIMARY KEY, date DATE NOT NULL, name VARCHAR(150) NOT NULL, comment VARCHAR(150) NOT NULL)')
@@ -39,11 +39,11 @@ def create_table():
 
 def create_post(date, name, comment):
     conn = psycopg2.connect(    
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
+            host = os.environ['HOST_NAME'],
             database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            user = os.environ['USERNAME'],
+            password = os.environ['PASSWORD']
+            port = os.environ['PORT']
     )
     cur = conn.cursor()
     cur.execute("INSERT INTO testdb (date, name, comment) VALUES (%s, %s, %s)", ('NOW()', name, comment)) 
@@ -53,11 +53,11 @@ def create_post(date, name, comment):
 
 def get_posts():
     conn = psycopg2.connect(
-        host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
+        host = os.environ['HOST_NAME'],
         database = 'd8hto9mvtubuln',
-        user = 'kmdvxvuvocjhha',
-        password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-        port = 5432
+        user = os.environ['USERNAME'],
+        password = os.environ['PASSWORD']
+        port = os.environ['PORT']
     )
     cur = conn.cursor()
     cur.execute("SELECT * FROM testdb")
