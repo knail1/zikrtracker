7f701ca7a30287d33fa8fce1d332e325bf3e0590
Dawud Rehan <d.rehan7@gmail.com>
2020-07-22 18:07:38 -0400

    slight tweak

--- models.py
+++ models.py
@@ -1,24 +1,25 @@
 import psycopg2
 import os
 
-#hello
+# hello
+
 
 def delete_table():
     try:
         conn = psycopg2.connect(
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
-            database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            host='ec2-18-209-187-54.compute-1.amazonaws.com',
+            database='d8hto9mvtubuln',
+            user='kmdvxvuvocjhha',
+            password='ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
+            port=5432
         )
     except:
-        conn = psycopg2.connect(    
-            host = os.environ['DB_HOST'],
-            database = 'd8hto9mvtubuln',
-            user = os.environ['USER_NAME'],
-            password = os.environ['PASSWORD'],
-            port = os.environ['DB_PORT']
+        conn = psycopg2.connect(
+            host=os.environ['DB_HOST'],
+            database=os.environ['DB'],
+            user=os.environ['USER_NAME'],
+            password=os.environ['PASSWORD'],
+            port=os.environ['DB_PORT']
         )
     cur = conn.cursor()
     cur.execute("DELETE FROM zikr")
@@ -30,69 +31,71 @@ def create_table():
 
     try:
         conn = psycopg2.connect(
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
-            database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            host='ec2-18-209-187-54.compute-1.amazonaws.com',
+            database='d8hto9mvtubuln',
+            user='kmdvxvuvocjhha',
+            password='ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
+            port=5432
         )
     except:
-        conn = psycopg2.connect(    
-            host = os.environ['DB_HOST'],
-            database = 'd8hto9mvtubuln',
-            user = os.environ['USER_NAME'],
-            password = os.environ['PASSWORD'],
-            port = os.environ['DB_PORT']
+        conn = psycopg2.connect(
+            host=os.environ['DB_HOST'],
+            database=os.environ['DB'],
+            user=os.environ['USER_NAME'],
+            password=os.environ['PASSWORD'],
+            port=os.environ['DB_PORT']
         )
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS zikr (date DATE NOT NULL PRIMARY KEY, complete BOOLEAN NOT NULL, comment VARCHAR(150))")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS zikr (date DATE NOT NULL PRIMARY KEY, complete BOOLEAN NOT NULL, comment VARCHAR(150))")
     conn.commit()
     conn.close()
-
 
 
 
 def create_post(date, complete, comment):
     try:
         conn = psycopg2.connect(
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
-            database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            host='ec2-18-209-187-54.compute-1.amazonaws.com',
+            database='d8hto9mvtubuln',
+            user='kmdvxvuvocjhha',
+            password='ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
+            port=5432
         )
     except:
-        conn = psycopg2.connect(    
-            host = os.environ['DB_HOST'],
-            database = 'd8hto9mvtubuln',
-            user = os.environ['USER_NAME'],
-            password = os.environ['PASSWORD'],
-            port = os.environ['DB_PORT']
+        conn = psycopg2.connect(
+            host=os.environ['DB_HOST'],
+            database=os.environ['DB'],
+            user=os.environ['USER_NAME'],
+            password=os.environ['PASSWORD'],
+            port=os.environ['DB_PORT']
         )
     cur = conn.cursor()
 
-    cur.execute("INSERT INTO zikr (date, complete, comment) VALUES (%s, %s, %s)", (date, '1', comment)) 
+    cur.execute(
+        "INSERT INTO zikr (date, complete, comment) VALUES (%s, %s, %s)", (date, '1', comment))
 
     conn.commit()
 
     conn.close()
 
+
 def get_posts():
     try:
         conn = psycopg2.connect(
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
-            database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            host='ec2-18-209-187-54.compute-1.amazonaws.com',
+            database='d8hto9mvtubuln',
+            user='kmdvxvuvocjhha',
+            password='ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
+            port=5432
         )
     except:
-        conn = psycopg2.connect(    
-            host = os.environ['DB_HOST'],
-            database = 'd8hto9mvtubuln',
-            user = os.environ['USER_NAME'],
-            password = os.environ['PASSWORD'],
-            port = os.environ['DB_PORT']
+        conn = psycopg2.connect(
+            host=os.environ['DB_HOST'],
+            database=os.environ['DB'],
+            user=os.environ['USER_NAME'],
+            password=os.environ['PASSWORD'],
+            port=os.environ['DB_PORT']
         )
     cur = conn.cursor()
     try:
@@ -102,25 +105,27 @@ def get_posts():
     except:
         create_table()
 
+
 def show_status(date):
     try:
         conn = psycopg2.connect(
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
-            database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            host='ec2-18-209-187-54.compute-1.amazonaws.com',
+            database='d8hto9mvtubuln',
+            user='kmdvxvuvocjhha',
+            password='ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
+            port=5432
         )
     except:
-        conn = psycopg2.connect(    
-            host = os.environ['DB_HOST'],
-            database = 'd8hto9mvtubuln',
-            user = os.environ['USER_NAME'],
-            password = os.environ['PASSWORD'],
-            port = os.environ['DB_PORT']
+        conn = psycopg2.connect(
+            host=os.environ['DB_HOST'],
+            database=os.environ['DB'],
+            user=os.environ['USER_NAME'],
+            password=os.environ['PASSWORD'],
+            port=os.environ['DB_PORT']
         )
     cur = conn.cursor()
-    cur.execute("SELECT * FROM zikr WHERE complete='1' ORDER BY date DESC LIMIT 1")
+    cur.execute(
+        "SELECT * FROM zikr WHERE complete='1' ORDER BY date DESC LIMIT 1")
     results = cur.fetchall()
     return results
     conn.commit()
@@ -130,26 +135,26 @@ def show_status(date):
 def update_zikr(date):
     try:
         conn = psycopg2.connect(
-            host = 'ec2-18-209-187-54.compute-1.amazonaws.com',
-            database = 'd8hto9mvtubuln',
-            user = 'kmdvxvuvocjhha',
-            password = 'ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
-            port = 5432
+            host='ec2-18-209-187-54.compute-1.amazonaws.com',
+            database='d8hto9mvtubuln',
+            user='kmdvxvuvocjhha',
+            password='ac9a4385919971b6c4d5695d7dce03df8a45e3cc9a8f31f78a985593754222f3',
+            port=5432
         )
     except:
-        conn = psycopg2.connect(    
-            host = os.environ['DB_HOST'],
-            database = 'd8hto9mvtubuln',
-            user = os.environ['USER_NAME'],
-            password = os.environ['PASSWORD'],
-            port = os.environ['DB_PORT']
+        conn = psycopg2.connect(
+            host=os.environ['DB_HOST'],
+            database=os.environ['DB'],
+            user=os.environ['USER_NAME'],
+            password=os.environ['PASSWORD'],
+            port=os.environ['DB_PORT']
         )
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM zikr WHERE date = '{date}'")
     row = cur.fetchall()
     completed = row[0][1]
-    #if completed == True:
-        #cur.execute(f"UPDATE zikr set complete = '0' where date = '{date}'")
+    # if completed == True:
+    #cur.execute(f"UPDATE zikr set complete = '0' where date = '{date}'")
     if completed == False:
         cur.execute(f"UPDATE zikr SET complete = '1' where date = '{date}'")
     elif completed == True:
